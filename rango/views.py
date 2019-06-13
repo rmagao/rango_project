b840ea93faa731fe827919463eb6d432037397d7
Ryan Agao <rmagao@spectrumone.co>
2019-06-13 02:50:32 -0700

    Templates and Media Files

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Rango says hey there partner!</h1>')
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse('<h1><em>About Page</em></h1>')
--- rango_project/urls.py
+++ rango_project/urls.py
@@ -22,5 +22,6 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('rango.urls')),
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
