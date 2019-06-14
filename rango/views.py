55e3c47e08270c15eda54a6a161f2637b496fb12
Ryan Agao <rmagao@spectrumone.co>
2019-06-14 01:45:51 -0700

    Customise index interface

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    #context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #return render(request, 'rango/index.html', context=context_dict)
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
