7769fd16070a4168d749ee6f0587decdce31e2f3
Ryan Agao <rmagao@spectrumone.co>
2019-06-17 06:24:01 +0800

    changed path to rango/

--- rango/urls.py
+++ rango/urls.py
@@ -4,7 +4,7 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', views.rango, name='rango'),
-    path('about/', views.about, name='about'),
-    path('login/', views.login, name='login'),
-    path('category/<slug:category_name_slug>/', views.show_category, name='show_category')
+    path('rango/about/', views.about, name='about'),
+    path('rango/login/', views.login, name='login'),
+    path('rango/category/<slug:category_name_slug>/', views.show_category, name='show_category')
 ]
