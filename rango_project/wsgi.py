83862fccc2ea1a7d8ec7660283393529c9a6a506
Ryan Agao <rmagao@spectrumone.co>
2019-06-24 15:04:27 +0800

    Minor changes to templates

--- rango_project/wsgi.py
+++ rango_project/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+path = 'C:/Users/ASUS1/AppData/Local/Packages/CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc/LocalState/rootfs/home/asus1/rango_project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rango_project.settings')
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
